b3b8c1a147afe0ae3a47ab3edb96c1ecd16ea5e3
Syed hamza waqas <124258441+sydhamzawaqas@users.noreply.github.com>
2024-01-15 12:45:50 +0500

    Update Code

--- Project.py
+++ Project.py
@@ -2,8 +2,9 @@ import streamlit as st, pandas as pd, numpy as np, yfinance as yf
 import plotly.express as px
 from alpha_vantage.fundamentaldata import FundamentalData
 from stocknews import StockNews
+import plotly.graph_objects as go
 
-st.title("Stock Dashboard")
+st.title("Stock Analysis Dashboard")
 ticker = st.sidebar.text_input("Ticker")
 start_date = st.sidebar.date_input("Start Date")
 end_date = st.sidebar.date_input("End Date")
@@ -13,8 +14,26 @@ data
 fig = px.line(data,x = data.index, y = data['Adj Close'],title=ticker)
 
 st.plotly_chart(fig)
+fig_candle = go.Figure(data=[go.Candlestick(x=data.index,
+                                             open=data['Open'],
+                                             high=data['High'],
+                                             low=data['Low'],
+                                             close=data['Close'])])
+fig_candle.update_layout(title=f"{ticker} - Candlestick Chart")
+st.subheader("Candlestick Chart")
+st.plotly_chart(fig_candle)
 
-pricing_data, fundamental_data, news = st.tabs(["Pricing Data", "Fundamental Data","Top 10 News"])
+st.subheader("Bar Chart")
+fig_bar = px.bar(data, x=data.index, y=data['Volume'], title=f"{ticker} - Volume")
+st.plotly_chart(fig_bar)
+
+st.subheader("Scatter Chart")
+fig_scatter = px.scatter(data, x=data.index, y=data['Close'], title=f"{ticker} - Scatter Plot")
+st.plotly_chart(fig_scatter)
+
+
+
+pricing_data, news = st.tabs(["Pricing Data","Top 10 News"])
 
 with pricing_data:
     st.header("Price Movements")
@@ -30,27 +49,6 @@ with pricing_data:
     st.write("Standard Deviation is ",stdev*100,"%")
     st.write("Risk ADJ Return is ",annual_return/(stdev*100))
     
-    
-#8A77I41H5ZCOKI49
-with fundamental_data:
-    st.write("Fundamental")
-    key ="HTMYU6KRIGQN3BLT"
-    fd = FundamentalData(key,output_format = "pandas")
-    st.subheader("Balance Sheet")
-    balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
-    bs = balance_sheet.T[2:]
-    bs.columns = list(balance_sheet.T.iloc[0])
-    st.write(bs)
-    st.subheader("Income Statement")
-    income_statement = fd.get_income_statement_annual(ticker)[0]
-    is1 =income_statement.T[2:]
-    is1.column = list(income_statement.T.iloc[0])
-    st.write(is1)
-    st.subheader("Cash Flow Statement")
-    cash_flow = fd.get_cash_flow_annual(ticker)[0]
-    cf =cash_flow.T[2:]
-    cf.column = list(cash_flow.T.iloc[0])    
-    st.write(cf)   
     
 with news:
     st.write("News")
